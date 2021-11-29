ee2193d1ed5e6cf87d3c3e7ceb26c92c5d870e2c
Onam Chilwan <OnamC@asos.com>
2021-11-29 14:53:10 +0000

    Added controller so component tests are now passing. This is only basic test to ensure route has been created and we get an OK result back.
    Basic skeleton is in place we can now start adding more detail to the API.

---
+++ src/Pokemon.Rotomdex.Web.Api/Controllers/PokemonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokemon.Rotomdex.Web.Api.Controllers
+{
+    [Route("rotomdex/v1/pokemon")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult Get([FromRoute] string name)
+        {
+            return Ok();
+        }
+    }
+}
