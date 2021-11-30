2d01c1fde7841b8b690b2acae4fcb3cf94091c8f
Onam Chilwan <OnamC@asos.com>
2021-11-30 15:23:38 +0000

    Created integration test library so we can create contract tests between us and third party. This is to ensure our expectation are always met.

---
+++ tests/Pokemon.Rotomdex.IntegrationTests/Adapters/PokeApiAdapterTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Pokemon.Rotomdex.IntegrationTests.Adapters
+{
+    [TestFixture]
+    public class PokeApiAdapterTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
