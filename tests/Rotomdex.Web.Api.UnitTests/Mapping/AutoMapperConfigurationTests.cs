7d98258d13b9671f8873e551879ca9cada7138e6
Onam Chilwan <OnamC@asos.com>
2021-11-30 22:23:10 +0000

    Added unit tests for mapping profile.

---
+++ tests/Rotomdex.Web.Api.UnitTests/Mapping/AutoMapperConfigurationTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using Rotomdex.Web.Api.Configuration.Mapping;
+
+namespace Rotomdex.Web.Api.UnitTests.Mapping
+{
+    [TestFixture]
+    public class AutoMapperConfigurationTests
+    {
+        [Test]
+        public void Domain_Mapping_Profile_Configuration_Is_Valid()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DomainMappingProfile>();
+            });
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
