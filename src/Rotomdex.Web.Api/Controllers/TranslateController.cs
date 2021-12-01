9d34268b0d9563608f2079cd059f0a493f784982
Onam Chilwan <OnamC@asos.com>
2021-12-01 15:10:02 +0000

    Implemented controller to get tests passing. We have hardcoded this for now but further tests will make this more dynamic.

---
+++ src/Rotomdex.Web.Api/Controllers/TranslateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rotomdex.Web.Api.Models;
+
+namespace Rotomdex.Web.Api.Controllers
+{
+    [Route("rotomdex/v1/pokemon")]
+    public class TranslateController
+    {
+        [HttpPost]
+        [Route("translate")]
+        public IActionResult Post(TranslationRequest request)
+        {
+            return new OkObjectResult(new PokemonResponse
+            {
+                Habitat = "cave",
+                Name = "mewtwo",
+                DescriptionStandard = "Ye Olde Shakespeare Talk",
+                IsLegendary = true
+            });
+        }
+    }
+}
